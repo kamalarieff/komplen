e813e4903d6ee64cc3d3cd154672b04b84fea872
Kamal Arieff <kamal.arieff@mudah.my>
2021-09-13 22:19:37 +0800

    feat: set correct db password for dev

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :komplen, Komplen.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "mysecretpassword",
   database: "komplen_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
