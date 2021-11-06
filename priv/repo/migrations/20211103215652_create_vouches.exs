a7d3977202abf914e7cd397c90662a877590b2a2
Kamal Arieff <kamal.arieff@mudah.my>
2021-11-07 01:33:48 +0800

    docs: adding YOGHIRT so that it's easier for to find tips

--- priv/repo/migrations/20211103215652_create_vouches.exs
+++ priv/repo/migrations/20211103215652_create_vouches.exs
@@ -3,7 +3,7 @@ defmodule Komplen.Repo.Migrations.CreateVouches do
 
   def change do
     create table(:vouches) do
-      # This is how you add options to this macro
+      # YOGHIRT: This is how you add options to this macro
       # https://elixirforum.com/t/can-you-modify-a-columns-references-3-options-in-ecto/4443
       add :complaint_id, references(:complaints, on_delete: :nothing), null: false
       add :user_id, references(:users, on_delete: :nothing), null: false
