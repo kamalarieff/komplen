c0ceb861e8bb691cba7540b4ad3970d9514c9c24
Kamal Arieff <kamal.arieff@mudah.my>
2021-11-09 22:50:17 +0800

    improvement: add seeding code to make starting the project easier

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,26 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+# YOGHIRT Got the idea of seeding the database from here
+# https://youtu.be/1YzAztAMgP4?t=2871
+# https://elixirforum.com/t/seeding-database-with-relationships/13240/2
+alias Komplen.Accounts.{User, Profile}
+alias Komplen.Complaints.Complaint
+
+user =
+  Komplen.Repo.insert!(%User{
+    username: "seed username",
+    profile: %Profile{
+      name: "seed name",
+      phone: "seed phone",
+      email: "seed email",
+      ic_number: "seed ic_number"
+    }
+  })
+
+Komplen.Repo.insert!(%Complaint{
+  body: "seed body",
+  title: "seed title",
+  user_id: user.id
+})
