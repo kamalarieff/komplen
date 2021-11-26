87b0e98e7c24af7d4db13ae3d00f768271d58c8b
Kamal Arieff <kamal.arieff@mudah.my>
2021-11-26 23:06:55 +0800

    feat: use pubsub to broadcast chat message

--- lib/komplen_web/live/room_live/show.ex
+++ lib/komplen_web/live/room_live/show.ex
@@ -2,20 +2,61 @@ defmodule KomplenWeb.RoomLive.Show do
   use KomplenWeb, :live_view
 
   alias Komplen.Chat
+  alias Komplen.Chat.Message
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, socket}
+  def mount(_params, session, socket) do
+    user_id = Map.get(session, "user_id")
+    {:ok, assign(socket, :user_id, user_id)}
   end
 
   @impl true
   def handle_params(%{"id" => id}, _, socket) do
+    room = Chat.get_room!(id)
+    chat_changeset = Chat.change_message(%Message{room_id: room.id})
+    topic = topic(id)
+
+    KomplenWeb.Endpoint.subscribe(topic)
+
     {:noreply,
      socket
      |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:room, Chat.get_room!(id))}
+     |> assign(:room, room)
+     |> assign(:chat_changeset, chat_changeset)}
+  end
+
+  @impl true
+  def handle_event("validate_chat", %{"message" => message_params}, socket) do
+    changeset =
+      %Message{}
+      |> Chat.change_message(message_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign(socket, :chat_changeset, changeset)}
+  end
+
+  @impl true
+  def handle_event("save_chat", %{"message" => %{"message" => message}}, socket) do
+    room_id = socket.assigns.room.id
+    user_id = socket.assigns.user_id
+    topic = topic(room_id)
+    Chat.create_message(%{message: message, user_id: user_id, room_id: room_id})
+    KomplenWeb.Endpoint.broadcast(topic, "send_message", room_id)
+
+    {:noreply,
+     socket
+     |> push_redirect(to: Routes.room_show_path(socket, :show, room_id))}
+  end
+
+  @impl true
+  def handle_info(%{event: "send_message", payload: room_id}, socket) do
+    {:noreply,
+     socket
+     |> push_redirect(to: Routes.room_show_path(socket, :show, room_id))}
   end
 
   defp page_title(:show), do: "Show Room"
   defp page_title(:edit), do: "Edit Room"
+
+  defp topic(id), do: "room:#{id}"
 end
